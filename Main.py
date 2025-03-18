6cc19376999a5d54caa55950bc12c68b1f063365
“Martijn <martijn.wehrens@gmail.com>
2025-03-18 17:54:39 +0100

    plotting works properly

--- Main.py
+++ Main.py
@@ -63,8 +63,11 @@ os.makedirs(output_folder, exist_ok=True)
 MAPPING_CHANNELS = {'nucleus':0, 'ERK':1, 'PKA':2}
 nuclear_channel = MAPPING_CHANNELS['nucleus']
 
+# TO DO: THE LOOP BELOW IS TOO LONG, PERHAPS WE SHOULD SPLIT THE CODE INTO FUNCTIONS
+
 # loop over tif files in input directory
 # for each file, separately analyze and create a csv output file
+df_list=[]
 for file_path in glob(os.path.join(input_folder, "*.tif")):
     # file_path = glob(os.path.join(input_folder, "*.tif"))[0]
     
@@ -101,8 +104,7 @@ for file_path in glob(os.path.join(input_folder, "*.tif")):
     # TO DO: DO WE WANT SOME KIND OF BACKGROUND CORRECTION?
 
     # Now combine the segmentation with the intensity signals to calculate nuclear and cytoplasmic signals per cell
-    # nucleus_masks_tracked, cytoplasm_masks_tracked, image_stack_current
-    df_list={}
+    # nucleus_masks_tracked, cytoplasm_masks_tracked, image_stack_current    
     for thekey in list(MAPPING_CHANNELS.keys()):        
         # thekey=list(MAPPING_CHANNELS.keys())[1]
         
@@ -117,53 +119,64 @@ for file_path in glob(os.path.join(input_folder, "*.tif")):
         df_current['Ratio_nucleus_div_cytoplasm'] = df_current['Intensity_nucleus']/df_current['Intensity_cytoplasm']
         # Add key to the df
         df_current['Key'] = thekey
+        # Add sample filename to df
+        df_current['Sample'] = file_name
         
         # export current df
         df_current.to_csv(os.path.join(output_folder, f"{file_name}_{thekey}_results.csv"), index=False)
         df_current.to_excel(os.path.join(output_folder, f"{file_name}_{thekey}_results.xlsx"), index=False)
         
         # save current df to list
-        df_list[thekey] = df_current
+        df_list.append(df_current)
         
-    # concatenate all dfs
-    df_data_all = pd.concat(df_list)
+# concatenate all dfs
+df_data_all = pd.concat(df_list, ignore_index=True)
+# Save those too
+df_data_all.to_csv(os.path.join(output_folder, f"{file_name}_ALL_results.csv"), index=False)
+df_data_all.to_excel(os.path.join(output_folder, f"{file_name}_ALL_results.xlsx"), index=False)
 
 ################################################################################
 # Now create a plot of the signals
 
-# plot the data
-# (fully co-pilot generated suggestion)
-sns.set_theme(style="whitegrid")
-g = sns.FacetGrid(df_data_all, col="Key", col_wrap=2, height=4, sharey=False)
-g.map(sns.lineplot, "Frame", "Intensity_nucleus", marker="o", color="b")
-g.map(sns.lineplot, "Frame", "Intensity_cytoplasm", marker="o", color="r")
-# g.map(sns.lineplot, "Frame", "Ratio_nucleus_div_cytoplasm", marker="o", color="g")
-g.set_axis_labels("Time", "Intensity")
-g.add_legend()
-plt.show()
+# Optionally, simply load data
+# (This might be usefull if we split these two 
+# df_data_all = pd.read_csv(os.path.join(output_folder, f"{file_name}_ALL_results.csv"), index=False)
+
+# TO DO: CONVERT THE BELOW CODE TO FUNCTIONS
 
 # plot the data
-df_data_all.columns
+# df_data_all.columns
 sns.set_theme(style="whitegrid")
 g = sns.FacetGrid(df_data_all, col="Key", col_wrap=2, height=4, sharey=False)
-g.map(sns.lineplot, "Frame", "Intensity_nucleus", data=df_data_all.loc[df_data_all['Cell']!='all'], hue='Cell')
-g.map(sns.lineplot, "Frame", "Intensity_cytoplasm", linestyle='--', data=df_data_all.loc[df_data_all['Cell']!='all'], hue='Cell')
-g.map(sns.lineplot, "Frame", "Intensity_nucleus", data=df_data_all.loc[df_data_all['Cell']=='all'], color='black', units='Cell', estimator=None, linewidth=2)
-g.map(sns.lineplot, "Frame", "Intensity_cytoplasm", data=df_data_all.loc[df_data_all['Cell']=='all'], color='black', units='Cell', estimator=None, linewidth=2, linestyle='--')
-# g.map(sns.lineplot, "Frame", "Ratio_nucleus_div_cytoplasm", marker="o", color="g")
-g.set_axis_labels("Time", "Intensity")
+# Plot individual cells with hue='Cell'
+_=g.map(sns.lineplot, "Frame", "Intensity_nucleus", data=df_data_all.loc[df_data_all['Cell'] != 'all'], hue='Cell', legend=False)
+_=g.map(sns.lineplot, "Frame", "Intensity_cytoplasm", linestyle='--', data=df_data_all.loc[df_data_all['Cell'] != 'all'], hue='Cell', legend=False)
+# Plot the average (black lines)
+_=g.map(sns.lineplot, "Frame", "Intensity_nucleus", data=df_data_all.loc[df_data_all['Cell'] == 'all'], color='black', units='Cell', estimator=None, linewidth=2, label='Average Nucleus')
+_=g.map(sns.lineplot, "Frame", "Intensity_cytoplasm", data=df_data_all.loc[df_data_all['Cell'] == 'all'], color='black', units='Cell', estimator=None, linewidth=2, linestyle='--', label='Average Cytoplasm')
+# Add legend 
 g.add_legend()
-plt.show()
+# Save
+# plt.tight_layout()
+g.figure.savefig(os.path.join(output_folder, f"PLOT_{file_name}_Intensity_plot_nuc-cyto-separate.pdf"), dpi=300, bbox_inches='tight')
+plt.close(g.figure)
+
 
 # plot the data
-df_data_all.columns
+# df_data_all.columns
 sns.set_theme(style="whitegrid")
 g = sns.FacetGrid(df_data_all, col="Key", col_wrap=2, height=4, sharey=False)
 g.map(sns.lineplot, "Frame", "Ratio_nucleus_div_cytoplasm", data=df_data_all.loc[df_data_all['Cell']=='all'], color='black', units='Cell', estimator=None, linewidth=2)
 g.map(sns.lineplot, "Frame", "Ratio_nucleus_div_cytoplasm", data=df_data_all.loc[df_data_all['Cell']!='all'], hue='Cell')
 g.set_axis_labels("Time", "Intensity")
 g.add_legend()
-plt.show()
+# Save
+# plt.tight_layout()
+g.figure.savefig(os.path.join(output_folder, f"PLOT_{file_name}_Intensity_plot_nuc-cyto-ratio.pdf"), dpi=300, bbox_inches='tight')
+plt.close(g.figure)
+
+
+
 
 
 

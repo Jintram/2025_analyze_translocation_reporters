ead93260845c22a7ed5c61f5930f4acdbbf5c599
JuliandeSwart <juliandeswart@gmail.com>
2025-03-25 11:47:43 +0100

    Changed location of script, for running through command line

--- analyze_transl_rep.py
+++ analyze_transl_rep.py
@@ -22,7 +22,7 @@ import pandas as pd
 ################################################################################
 
 # sys.path.append("/Volumes/sils-mc/13776452/Python_scripts")
-sys.path.append('/Users/m.wehrens/Documents/git_repos/_UVA/2024_small-projects/2025_analyze_translocation_reporters_Julian/')
+sys.path.append('/Users/juliandeswart/Desktop/2025_analyze_translocation_reporters')
 
 import Functions.Segmentation as TRseg # Segmentation & tracking functions
     # import importlib; importlib.reload(TRseg)
